95fd9dd3f1977054f3e630c13541c834395e3d0c
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2024-04-17 11:01:01 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
+# setup ROOT
+echo $HOSTNAME
+if [[ $HOSTNAME = apcatlas01.in2p3.fr ]]; then
+    source /usr/local/root-6.30.06/install/bin/thisroot.sh
+elif [[ $HOSTNAME == lxplus*.cern.ch ]]; then
+    # expect root to be setup via key4hep
+    echo "ROOT should be setup via key4hep or LCG"
+else
+    # on my laptop
+    setroot
+fi
+    
 # root should be setup via key4hep
 export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${PWD}/dist
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/dist
