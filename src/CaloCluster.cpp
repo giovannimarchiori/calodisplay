32ef2719ff0b58172c36cb519ba29331cc23b2dc
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2025-05-05 14:27:06 +0200

    add muon info to caloclusters

--- src/CaloCluster.cpp
+++ src/CaloCluster.cpp
@@ -39,6 +39,16 @@ float CaloCluster::getEnergyHCal() const
   return E;
 }
 
+float CaloCluster::getEnergyMuon() const
+ {
+  float E(0.);
+  for (float Elayer : m_energyVsMuonLayer)
+  {
+    E += Elayer;
+  }
+  return E;
+}
+
 float CaloCluster::getEnergyInECalLayer(unsigned int layer) const
 {
   if (layer > m_energyVsECalLayer.size())
@@ -65,6 +75,19 @@ float CaloCluster::getEnergyInHCalLayer(unsigned int layer) const
   }
 }
 
+float CaloCluster::getEnergyInMuonLayer(unsigned int layer) const
+{
+  if (layer > m_energyVsMuonLayer.size())
+  {
+    cout << "CaloCluster::getEnergyInMuonLayer : layer outside of range, returning 0.0" << endl;
+    return 0;
+  }
+  else
+  {
+    return m_energyVsMuonLayer[layer];
+  }
+}
+
 void CaloCluster::setEnergyVsECalLayers(const std::vector<float> &energyVsECalLayer)
 {
   m_energyVsECalLayer = std::vector<float>(energyVsECalLayer); // use copy constructor
@@ -73,6 +96,11 @@ void CaloCluster::setEnergyVsECalLayers(const std::vector<float> &energyVsECalLa
 void CaloCluster::setEnergyVsHCalLayers(const std::vector<float> &energyVsHCalLayer)
 {
   m_energyVsHCalLayer = std::vector<float>(energyVsHCalLayer); // use copy constructor
+}
+
+void CaloCluster::setEnergyVsMuonLayers(const std::vector<float> &energyVsMuonLayer)
+{
+  m_energyVsMuonLayer = std::vector<float>(energyVsMuonLayer); // use copy constructor
 }
 
 TVector3 CaloCluster::getBarycenterInECalLayer(unsigned int layer) const
@@ -101,6 +129,19 @@ TVector3 CaloCluster::getBarycenterInHCalLayer(unsigned int layer) const
   }
 }
 
+TVector3 CaloCluster::getBarycenterInMuonLayer(unsigned int layer) const
+{
+  if (layer > m_barycenterVsMuonLayer.size())
+  {
+    cout << "CaloCluster::getEnergyInMuonLayer : layer outside of range, returning origin" << endl;
+    return TVector3(0.0, 0.0, 0.0);
+  }
+  else
+  {
+    return m_barycenterVsMuonLayer[layer];
+  }
+}
+
 void CaloCluster::setBarycenterVsECalLayers(const std::vector<TVector3> &barycenterVsECalLayer)
 {
   m_barycenterVsECalLayer = std::vector<TVector3>(barycenterVsECalLayer); // use copy constructor
@@ -111,6 +152,11 @@ void CaloCluster::setBarycenterVsHCalLayers(const std::vector<TVector3> &barycen
   m_barycenterVsHCalLayer = std::vector<TVector3>(barycenterVsHCalLayer); // use copy constructor
 }
 
+void CaloCluster::setBarycenterVsMuonLayers(const std::vector<TVector3> &barycenterVsMuonLayer)
+{
+  m_barycenterVsMuonLayer = std::vector<TVector3>(barycenterVsMuonLayer); // use copy constructor
+}
+
 void CaloCluster::print() const
 {
   std::cout << "Cluster: " << m_index << std::endl;
@@ -118,6 +164,7 @@ void CaloCluster::print() const
   std::cout << "Barycenter (r, theta, phi): " << m_barycenter.Pt() << " " << m_barycenter.Theta() << " " << m_barycenter.Phi() << std::endl;
   std::cout << "Energy in ECAL: " << getEnergyECal() << " GeV" << endl;
   std::cout << "Energy in HCAL: " << getEnergyHCal() << " GeV" << endl;
+  std::cout << "Energy in MUON: " << getEnergyMuon() << " GeV" << endl;
   std::cout << "Energy and barycenter in each layer of the ECAL: " << std::endl;
   for (unsigned int i=0; i<m_energyVsECalLayer.size(); i++)
   {
@@ -127,6 +174,11 @@ void CaloCluster::print() const
   for (unsigned int i=0; i<m_energyVsHCalLayer.size(); i++)
   {
     std::cout << i << "   " << m_energyVsHCalLayer[i] << " GeV  "  <<   m_barycenterVsHCalLayer[i].Pt() << " " << m_barycenterVsHCalLayer[i].Theta() << " " << m_barycenterVsHCalLayer[i].Phi() << std::endl;
+  }
+  std::cout << "Energy and barycenter in each layer of the MUON: " << std::endl;
+  for (unsigned int i=0; i<m_energyVsMuonLayer.size(); i++)
+  {
+    std::cout << i << "   " << m_energyVsMuonLayer[i] << " GeV  "  <<   m_barycenterVsMuonLayer[i].Pt() << " " << m_barycenterVsMuonLayer[i].Theta() << " " << m_barycenterVsMuonLayer[i].Phi() << std::endl;
   }
   // ideally divide by cm (or mm)
   // and report output in cm
